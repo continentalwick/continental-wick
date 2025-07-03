139f729902b9c411d967b1a3db50cb10dfff342f
continentalwick <wicktokenhq@gmail.com>
2025-07-02 23:03:06 -0700

    Create create-payload.js

---
+++ api/create-payload.js
@@ -0,0 +1,23 @@
+export default async function handler(req, res) {
+  const response = await fetch("https://xumm.app/api/v1/platform/payload", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+      "X-API-Key": "YOUR_XUMM_API_KEY"
+    },
+    body: JSON.stringify({
+      txjson: {
+        TransactionType: "TrustSet",
+        LimitAmount: {
+          currency: "WCK",
+          issuer: "rEtKBaH1koMJtQiqdhDNgS4ZrUNsJKc5go",
+          value: "100000000"
+        }
+      }
+    })
+  });
+
+  const data = await response.json();
+  res.status(200).json(data);
+}
